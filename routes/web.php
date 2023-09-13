365083988b6c3ccb7c713a397c542fa3312d26ac
JesminNancy <jesminnancy@hotmail.com>
2023-09-13 15:12:59 +0600

    Admin Login Email Setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\AdminLoginController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/admin/home',[AdminDashboardController::class,'index'])->name('admin.home');
+Route::get('/admin/login',[AdminLoginController::class,'index'])->name('admin.login');
+Route::get('/admin/forget-password',[AdminLoginController::class,'forget_password'])->name('admin.forget_password');
