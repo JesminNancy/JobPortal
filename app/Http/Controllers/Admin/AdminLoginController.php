e1725991453686b0e59676c73e7b9b3df5b81cb3
JesminNancy <jesminnancy@hotmail.com>
2023-09-16 13:37:40 +0600

    Reset password complete

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -39,6 +39,26 @@ class AdminLoginController extends Controller
         Mail::to($request->email)->send(new WebsiteMail($subject,$message));
         return redirect()->route('admin_login')->with('success','Please check your email and follow the steps there!');
     }
+    public function reset_password($token,$email){
+        $admin_data = Admin::where('token',$token)->where('email',$email)->first();
+        if(!$admin_data){
+            return redirect()->route('admin_login');
+        }
+         return view('admin.reset_password',compact('token','email'));
+    }
+    public function reset_password_submit(Request $request){
+        $request->validate([
+            'password' => 'required',
+            'retype_password' => 'required|same:password'
+        ]);
+
+        $admin_data = Admin::where('token',$request->token)->where('email',$request->email)->first();
+
+        $admin_data->password = Hash::make($request->password);
+        $admin_data->token = '';
+        $admin_data->update();
+        return redirect()->route('admin_login')->with('success','Password is set successfully');
+    }
     public function login_submit(Request $request){
         $request->validate([
             'email' => 'required|email',
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,6 @@ Route::get('/admin/forget-password',[AdminLoginController::class,'forget_passwor
 Route::post('/admin/forget-password/submit',[AdminLoginController::class,'forget_password_submit'])->name('admin_forget_password_submit');
 Route::post('/admin/login-submit',[AdminLoginController::class,'login_submit'])->name('admin_login_submit');
 Route::get('/admin/logout',[AdminLoginController::class,'logout'])->name('admin_logout');
+
+Route::get('admin/reset_password/{token}/{email}',[AdminLoginController::class,'reset_password'])->name('admin_reset_password');
+Route::post('/admin/reset_password_submit',[AdminLoginController::class,'reset_password_submit'])->name('admin_reset_password_submit');
